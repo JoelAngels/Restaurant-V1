8d64e6b1817c2e524f2ff6b5c5854a577f54c14a
JoelAngels <joelbaraka4325@gmail.com>
2021-02-08 21:53:14 +0300

    Added the Hero Container Details

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,32 @@
 import React from "react";
 import Navbar from "../Navbar";
-import { HeroContainer } from "./HeroElements";
+import {
+  HeroContainer,
+  HeroItems,
+  HeroContent,
+  Heroh1,
+  Heroh2,
+  HeroP,
+  HeroBtn,
+  Heroh5,
+} from "./HeroElements";
 
 const Hero = () => {
   return (
     <HeroContainer>
       <Navbar />
+      <HeroContent>
+        <HeroItems>
+          <Heroh5>Welcome</Heroh5>
+          <Heroh1>PRIME STEAK RESTAURANT</Heroh1>
+          <HeroP>Book Table Directly</HeroP>
+          <Heroh2>(215) - 468 - 6550</Heroh2>
+          <HeroP>
+            Opening Hours Mon-Fri: 6:00pm - 10:00pm, Weekends: 8:00pm - 10:00pm
+          </HeroP>
+          <HeroBtn>Place Order</HeroBtn>
+        </HeroItems>
+      </HeroContent>
     </HeroContainer>
   );
 };
