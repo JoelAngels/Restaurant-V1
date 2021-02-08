7a4cd1d57d0e382463e500b6fb73a848bb298f43
JoelAngels <joelbaraka4325@gmail.com>
2021-02-08 17:51:10 +0300

    Added the router, icons, and layout for the NAVBAR

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,4 +1,46 @@
 //styled components is a library that allows you to mix js and css using a technique called css-in-js
-import styled from "styled-components";
 
-export const Nav = styled.nam;
+import styled from "styled-components";
+import { NavLink as Link } from "react-router-dom";
+import {GiSteak} from "react-icons/gi";
+
+export const Nav = styled.nav`
+  background: red;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  font-weight: 700;
+`;
+
+export const NavLink = styled(Link)`
+    color: #fff;
+    font-size: 2rem;
+    display: flex;
+    align-items: center
+    text-decoration: none;
+    curosr: pointer;
+
+    @media screen and (max-width: 400px) {
+        position: absolute;
+        top: 10px;
+        left: 25px;
+    }
+`;
+
+export const NavIcon = styled.div`
+display: block;
+position: absolute;
+top: 0;
+right: 0;
+cursor: pointer;
+color: #fff;
+
+p {
+  transform: translate(-175%, 100%)
+  font-weight: bold;
+}
+`;
+export const Bars = styled(GiSteak)`
+font-size: 2rem,
+transform: translate(-50%, -15%);
+`;
