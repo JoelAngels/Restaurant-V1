0d79cb758292c078fa1887969825157a1136088e
JoelAngels <joelbaraka4325@gmail.com>
2021-02-09 21:15:38 +0300

    Finished styling the Products Section

---
+++ src/components/Products/data.js
@@ -0,0 +1,30 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: "Steak",
+    name: "CHICAGO'S BEST SELLER",
+    desc: "Filet Mignons,Boneless Strips, Sirloins, Ribeyes, Arsley Flakes, 'Contains Wheat',",
+    price: "$199.95",
+    button: "Add to Cart"
+  },
+  {
+    img: product2,
+    alt: "Steak",
+    name: "Grilled Butter-Marinated  Steaks",
+    desc: "Dry Aged Tomahawk Ribeyes, Steak Burgers,Bottle Chicago Steak Company Seasoning",
+    price: "$284.95",
+    button: "Add to Cart"
+  },
+  {
+    img: product3,
+    alt: "Steak",
+    name: "Marinated In Exsistance",
+    desc: "Premium Angus Beef Filet Mignons,Maryland Crab Cakes, Garlic powder",
+    price: "$174.95",
+    button: "Add to Cart"
+  },
+];
